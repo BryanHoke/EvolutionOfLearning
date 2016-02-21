813053c849cc91d5213583c1da2d6b3abc70ee89
Bryan Hoke <bryan.hoke@nextthought.com>
2016-02-21 17:43:48 -0600

    Added DataFetcher.

---
+++ EvolutionOfLearning/DataFetcher.swift
@@ -0,0 +1,15 @@
+//
+//  DataFetcher.swift
+//  EvolutionOfLearning
+//
+//  Created by Bryan Hoke on 2/20/16.
+//  Copyright © 2016 Bryan Hoke. All rights reserved.
+//
+
+import Foundation
+
+protocol DataFetcher {
+	
+	
+	
+}
