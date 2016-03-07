540fd75e98e6d2a058e7d11163746cfabf9a2d94
Bryan Hoke <bryan.hoke@nextthought.com>
2016-03-06 23:19:08 -0600

    Added the Evolution struct.

---
+++ EvolutionOfLearning/Evolution.swift
@@ -0,0 +1,19 @@
+//
+//  Evolution.swift
+//  EvolutionOfLearning
+//
+//  Created by Bryan Hoke on 3/6/16.
+//  Copyright © 2016 Bryan Hoke. All rights reserved.
+//
+
+import Foundation
+
+public struct Evolution {
+	
+	let environment: EvolutionaryEnvironment
+	
+	let numberOfGenerations: Int
+	
+	let numberOfTrials: Int
+	
+}
