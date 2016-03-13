c165f8ba1900b15c3c3deabc5cfc6faeb5edf7aa
Bryan Hoke <bryan.hoke@nextthought.com>
2016-03-13 16:08:15 -0500

    Updated ExperimentConfig.

--- EvolutionOfLearning/ExperimentConfig.swift
+++ EvolutionOfLearning/ExperimentConfig.swift
@@ -10,9 +10,27 @@ import Foundation
 
 public struct ExperimentConfig {
 	
-	public var chromosomeSize = 35
+	public var evolutionaryTaskCount = 20
+	
+	public var testTaskCount = 10
+	
+	public var fitnessConfig = FitnessConfig()
+	
+	public var geneticAlgorithmConfig = GeneticAlgorithmConfig()
+	
+}
+
+public struct FitnessConfig {
+	
+	public var learningRuleSize = 35
 	
 	public var populationSize = 40
+	
+	public var numberOfTrainingEpochs = 10
+	
+}
+
+public struct GeneticAlgorithmConfig {
 	
 	public var elitismCount = 1
 	
@@ -20,8 +38,6 @@ public struct ExperimentConfig {
 	
 	public var mutationRate = 0.01
 	
-	public var numberOfTrainingEpochs = 10
-	
-	public var taskCount = 20
+	public var numberOfGenerations = 1000
 	
 }
