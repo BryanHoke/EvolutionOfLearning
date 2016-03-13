b7e3b690653cd63b0b639831ca311afc67720fbc
Bryan Hoke <bryan.hoke@nextthought.com>
2016-03-12 22:41:33 -0600

    Implement ChalmersTrial.

--- EvolutionOfLearning/ChalmersTrial.swift
+++ EvolutionOfLearning/ChalmersTrial.swift
@@ -12,10 +12,56 @@ public typealias FitnessAgentFactory = (tasks: [Task]) -> FitnessAgent
 
 public struct ChalmersTrial {
 	
-	public let dataManager: DataManager
+	public var evolutionaryTasks: [Task]
 	
-	public let evolution: Evolution
+	public var testTasks: [Task]
 	
-	public let learningTest: LearningTest
+	public func run() -> ChalmersTrialRecord {
+		let evolutionRecord = runEvolution()
+		
+		let testRecord = runLearningTest(with: evolutionRecord.history)
+		
+		return ChalmersTrialRecord(evolutionRecord: evolutionRecord, learningTestRecord: testRecord)
+	}
 	
+	private func runEvolution() -> EvolutionRecord {
+		let fitness = makeFitnessAgent(with: evolutionaryTasks)
+		let reproduction = ChalmersReproductionAgent(elitismCount: 1, mutationRate: 0.01, crossoverRate: 0.8)
+		let environment = Environment(populationSize: 40, fitnessAgent: fitness, reproductionAgent: reproduction)
+		let evolution = Evolution(environment: environment, numberOfGenerations: 1000)
+		return evolution.run()
+	}
+	
+	private func runLearningTest(with history: [Population]) -> LearningTestRecord {
+		let fitness = makeFitnessAgent(with: testTasks)
+		let learningTest = LearningTest(fitnessAgent: fitness, history: history)
+		return learningTest.run()
+	}
+	
+	private func makeFitnessAgent(with tasks: [Task]) -> FitnessAgent {
+		return ChalmersFitnessAgent(learningRuleSize: 35, numberOfTrainingEpochs: 10, tasks: tasks)
+	}
+	
+}
+
+public struct ChalmersTrialRecord {
+	
+	public var evolutionRecord: EvolutionRecord
+	
+	public var learningTestRecord: LearningTestRecord
+	
+}
+
+// TODO: Relocate this when needed
+private func bifurcate(tasks: [Task], evolutionaryTaskCount count: Int) -> (evolutionary: [Task], test: [Task]) {
+	var tasks = tasks
+	var evolutionaryTasks: [Task] = []
+	
+	for _ in 0..<count {
+		let index = Int(arc4random_uniform(UInt32(tasks.count)))
+		let task = tasks.removeAtIndex(index)
+		evolutionaryTasks.append(task)
+	}
+	
+	return (evolutionary: evolutionaryTasks, test: tasks)
 }
