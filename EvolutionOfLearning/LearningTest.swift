6420e92f94bd14506d3b9280c5b0bd0855c72a70
Bryan Hoke <bryan.hoke@nextthought.com>
2016-03-12 22:38:14 -0600

    LearningTest now computes the most fit individuals in a population.

--- EvolutionOfLearning/LearningTest.swift
+++ EvolutionOfLearning/LearningTest.swift
@@ -8,18 +8,38 @@
 
 import Foundation
 
-
-
 public struct LearningTest {
 	
-	let fitnessAgent: FitnessAgent
+	public var fitnessAgent: FitnessAgent
 	
-	let fetcher: DataFetcher
+	public var history: [Population]
 	
-	func computeTestFitness() -> LearningTestRecord {
-		let chromosome = fetcher.fetchMostFitChromosome()
+	func run() -> LearningTestRecord {
+		let mostFitIndividual = findMostFitIndividual()
+		let chromosome = mostFitIndividual.chromosome
 		let fitness = fitnessAgent.fitness(of: chromosome)
 		return LearningTestRecord(chromosome: chromosome, fitness: fitness, tasks: fitnessAgent.tasks)
+	}
+	
+	/// - note: Assumes `Population`s in `history` are sorted by `fitness`.
+	public func findMostFitIndividual() -> Individual {
+		guard !history.isEmpty else {
+			preconditionFailure("History must not be empty")
+		}
+		var mostFitIndividual: Individual?
+		for population in history {
+			guard let mostFitMember = population.first else {
+				preconditionFailure("No population should be empty")
+			}
+			guard let currentMostFitIndividual = mostFitIndividual else {
+				mostFitIndividual = mostFitMember
+				continue
+			}
+			if mostFitMember.fitness > currentMostFitIndividual.fitness {
+				mostFitIndividual = mostFitMember
+			}
+		}
+		return mostFitIndividual!
 	}
 	
 }
