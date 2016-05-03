10fc267d3e5c2570c8dc671cc73ee53b525c7e78
Bryan Hoke <bryan.hoke@nextthought.com>
2016-05-03 00:28:05 -0500

    Implement `ConfigurationView`.

---
+++ EvolutionOfLearning/Configuration/ConfigurationView.swift
@@ -0,0 +1,52 @@
+//
+//  ConfigurationView.swift
+//  EvolutionOfLearning
+//
+//  Created by Bryan Hoke on 5/1/16.
+//  Copyright © 2016 Bryan Hoke. All rights reserved.
+//
+
+import Cocoa
+
+class ConfigurationView: NSView {
+	
+	override init(frame frameRect: NSRect) {
+		stackView = NSStackView(views: [elitismField, crossoverField, mutationField, populationSizeField, generationsField, trialsField, runButton])
+		
+		super.init(frame: frameRect)
+		
+		addSubview(stackView)
+		stackView.frame = bounds
+		
+		stackView.autoresizingMask = [.ViewWidthSizable, .ViewHeightSizable]
+		
+//		stackView.translatesAutoresizingMaskIntoConstraints = false
+//		stackView.topAnchor.constraintEqualToAnchor(topAnchor).active = true
+//		stackView.bottomAnchor.constraintEqualToAnchor(bottomAnchor).active = true
+//		stackView.leadingAnchor.constraintEqualToAnchor(leadingAnchor).active = true
+//		stackView.trailingAnchor.constraintEqualToAnchor(trailingAnchor).active = true
+		
+		stackView.orientation = .Vertical
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	let elitismField = NSTextField()
+	
+	let crossoverField = NSTextField()
+	
+	let mutationField = NSTextField()
+	
+	let populationSizeField = NSTextField()
+	
+	let generationsField = NSTextField()
+	
+	let trialsField = NSTextField()
+	
+	let runButton = NSButton()
+	
+	private let stackView: NSStackView
+    
+}
