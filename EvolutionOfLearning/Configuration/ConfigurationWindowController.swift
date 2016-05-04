cb2b4c70a16a8c3872289c04aa94812e9557d912
Bryan Hoke <bryan.hoke@nextthought.com>
2016-05-03 19:40:56 -0500

    Implement `ConfigurationWindowController`.

--- EvolutionOfLearning/Configuration/ConfigurationWindowController.swift
+++ EvolutionOfLearning/Configuration/ConfigurationWindowController.swift
@@ -9,7 +9,40 @@
 import Cocoa
 
 class ConfigurationWindowController: NSWindowController {
+	
+	weak var eventHandler: ConfigurationEventHandling?
 
+	@IBOutlet weak var conditionButton: NSPopUpButton!
+	
+	@IBOutlet weak var generationsField: NSTextField!
+	
+	@IBOutlet weak var trialsField: NSTextField!
+	
+	@IBOutlet weak var runButton: NSButton!
+	
+	@IBAction func runButtonClicked(sender: NSButton) {
+		eventHandler?.runButtonPressed()
+	}
+	
+	@IBAction func conditionButtonChanged(sender: NSPopUpButton) {
+		eventHandler?.selectedConditionChanged(to: sender.titleOfSelectedItem!)
+	}
+	
+	override func controlTextDidChange(obj: NSNotification) {
+		guard let textField = obj.object as? NSTextField else {
+			return
+		}
+		
+		let value = textField.integerValue
+		
+		if textField === generationsField {
+			eventHandler?.numberOfGenerationsChanged(to: value)
+		}
+		else if textField === trialsField {
+			eventHandler?.numberOfTrialsChanged(to: value)
+		}
+	}
+	
     override func windowDidLoad() {
         super.windowDidLoad()
 
