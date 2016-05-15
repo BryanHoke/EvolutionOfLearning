64f8056993a95606de4209f2963ec3f1db22139a
Bryan Hoke <bryan.hoke@nextthought.com>
2016-05-15 14:40:41 -0500

    Implement `TableScanner`.

---
+++ EvolutionOfLearning/TableScanner.swift
@@ -0,0 +1,46 @@
+//
+//  TableScanner.swift
+//  EvolutionOfLearning
+//
+//  Created by Bryan Hoke on 5/15/16.
+//  Copyright © 2016 Bryan Hoke. All rights reserved.
+//
+
+import Foundation
+
+final class TableScanner {
+	
+	static let sharedScanner = TableScanner()
+	
+	func scanTable(fromFileAtPath path: String) throws -> [[String]] {
+		var table = [[String]]()
+		
+		let fileContent = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+		
+		let lines: [String] = fileContent.componentsSeparatedByCharactersInSet(.newlineCharacterSet())
+		
+		for line in lines {
+			let tokens: [String] = line.componentsSeparatedByString(",")
+			table.append(tokens)
+		}
+		
+		return table
+	}
+	
+	func scanValues(fromFilesAtPaths paths: [String], rowIndices: [Int], columnIndices: [Int]) throws -> [[[String]]] {
+		var values: [[[String]]] = .init(count: columnIndices.count, repeatedValue: .init(count: rowIndices.count, repeatedValue: []))
+		
+		for path in paths {
+			let table = try scanTable(fromFileAtPath: path)
+			
+			for (i, row) in rowIndices.enumerate() {
+				for (j, col) in columnIndices.enumerate() {
+					values[i][j].append(table[row][col])
+				}
+			}
+		}
+		
+		return values
+	}
+	
+}
