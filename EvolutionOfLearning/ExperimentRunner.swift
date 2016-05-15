30c681e1f309a5466181d1e24163386c9951f9c6
Bryan Hoke <bryan.hoke@nextthought.com>
2016-05-15 15:27:28 -0500

    Add `ExperimentRunner`.

---
+++ EvolutionOfLearning/ExperimentRunner.swift
@@ -0,0 +1,25 @@
+//
+//  ExperimentRunner.swift
+//  EvolutionOfLearning
+//
+//  Created by Bryan Hoke on 5/15/16.
+//  Copyright © 2016 Bryan Hoke. All rights reserved.
+//
+
+import Foundation
+
+class ExperimentRunner {
+	
+	var config = ExperimentConfig()
+	
+	var condition: ExperimentalCondition = .learningRuleEvolution
+	
+	let allConditions = ExperimentalCondition.allConditions
+	
+	var numberOfTrials = 10
+	
+	func runExperiment() {
+		
+	}
+	
+}
