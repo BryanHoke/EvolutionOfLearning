d49060929d013e60f516f192030cf066ed09e98a
Bryan Hoke <bryan.hoke@nextthought.com>
2016-05-16 12:36:02 -0500

    Add `CSVRecordPersister`.

---
+++ EvolutionOfLearning/CSVRecordPersister.swift
@@ -0,0 +1,17 @@
+//
+//  CSVRecordPersister.swift
+//  EvolutionOfLearning
+//
+//  Created by Bryan Hoke on 5/16/16.
+//  Copyright © 2016 Bryan Hoke. All rights reserved.
+//
+
+import Foundation
+
+class CSVRecordPersister: RecordPersisting {
+	
+	func persist(record: ExperimentRecord) {
+		
+	}
+	
+}
