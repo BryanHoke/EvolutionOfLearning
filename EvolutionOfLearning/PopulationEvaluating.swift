2870ffc0b59f241d2758ffa849ae7283c87551af
Bryan Hoke <bryan.hoke@nextthought.com>
2016-05-17 18:47:36 -0500

    Revise to handle concurrency properly.

--- EvolutionOfLearning/PopulationEvaluating.swift
+++ EvolutionOfLearning/PopulationEvaluating.swift
@@ -14,24 +14,35 @@ public protocol PopulationEvaluating {
 	
 }
 
-public struct ConcurrentPopulationEvaluator: PopulationEvaluating {
+public class ConcurrentPopulationEvaluator: PopulationEvaluating {
+	
+	public init(fitnessFunc: FitnessFunc) {
+		self.fitnessFunc = fitnessFunc
+	}
 	
 	public let fitnessFunc: FitnessFunc
 	
+	private var fitnesses: [Double] = []
+	
 	public func evaluate(inout population: Population) {
+		fitnesses = Array<Double>(count: population.count, repeatedValue: 0)
 		let blocks = makeDispatchBlocks(forEvaluating: &population)
 		concurrentlyDispatch(blocks, priority: DISPATCH_QUEUE_PRIORITY_HIGH)
+		for (index, fitness) in fitnesses.enumerate() {
+			population[index].fitness = fitness
+		}
 	}
 	
 	private func makeDispatchBlocks(inout forEvaluating population: Population) -> [dispatch_block_t] {
 		return population.indices.map { index -> dispatch_block_t in
-			self.makeBlockToEvaluateFitness(of: &population[index])
+			self.makeBlockToEvaluateFitness(of: &population[index], atIndex: index)
 		}
 	}
 	
-	private func makeBlockToEvaluateFitness(inout of member: Individual) -> dispatch_block_t {
+	private func makeBlockToEvaluateFitness(inout of member: Individual, atIndex index: Int) -> dispatch_block_t {
 		return {
 			self.evaluateFitness(of: &member)
+			self.fitnesses[index] = member.fitness
 		}
 	}
 	
