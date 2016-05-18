14a0ba6bd6158831cdb4716083f98ea74326a9f7
Bryan Hoke <bryan.hoke@nextthought.com>
2016-05-17 19:04:31 -0500

    Add 	`TextRecordPersister`.

---
+++ EvolutionOfLearning/TextRecordPersister.swift
@@ -0,0 +1,21 @@
+//
+//  TextRecordPersister.swift
+//  EvolutionOfLearning
+//
+//  Created by Bryan Hoke on 5/17/16.
+//  Copyright © 2016 Bryan Hoke. All rights reserved.
+//
+
+import Foundation
+
+class TextRecordPersister : RecordPersisting {
+	
+	var resultsPath: String {
+		return "/Users/bryanhoke/Projects/BDHSoftware/EvolutionOfLearning/Results/"
+	}
+	
+	func persist(record: ExperimentRecord) {
+		
+	}
+	
+}
