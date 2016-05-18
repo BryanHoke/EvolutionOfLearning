5ef8e2bf90486cf3d99bd08c8e19f62d02f36fdc
Bryan Hoke <bryan.hoke@nextthought.com>
2016-05-17 19:26:19 -0500

    Add `ConfigFileWriter`.

---
+++ EvolutionOfLearning/ConfigFileWriter.swift
@@ -0,0 +1,13 @@
+//
+//  ConfigFileWriter.swift
+//  EvolutionOfLearning
+//
+//  Created by Bryan Hoke on 5/17/16.
+//  Copyright © 2016 Bryan Hoke. All rights reserved.
+//
+
+import Foundation
+
+final class ConfigFileWriter {
+	
+}
