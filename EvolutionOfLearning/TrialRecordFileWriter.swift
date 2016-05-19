60be6d161d7574313e0b4dbd4a1046522040aa94
Bryan Hoke <bryan.hoke@nextthought.com>
2016-05-18 21:46:34 -0500

    Implement `TrialRecordFileWriter`.

---
+++ EvolutionOfLearning/TrialRecordFileWriter.swift
@@ -0,0 +1,56 @@
+//
+//  TrialRecordFileWriter.swift
+//  EvolutionOfLearning
+//
+//  Created by Bryan Hoke on 5/18/16.
+//  Copyright © 2016 Bryan Hoke. All rights reserved.
+//
+
+import Foundation
+
+final class TrialRecordFileWriter {
+	
+	var baseFilename: String {
+		return "Trial"
+	}
+	
+	func persist(record: TrialRecord, withIndex index: Int, inDirectory directoryPath: String) {
+		let content = makeFileContent(for: record)
+		let path = "\(directoryPath)\(baseFilename)\(index)"
+		do {
+			try content.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding)
+		}
+		catch let error as NSError {
+			preconditionFailure("\(error)")
+		}
+	}
+	
+	func makeFileContent(for record: TrialRecord) -> String {
+		return record.evaluations.map(makeFileContent(for:)).joinWithSeparator("\n\n")
+	}
+	
+	func makeFileContent(for evaluation: EvaluationRecord) -> String {
+		return "\(evaluation.name)\n"
+		+ "Tasks: \(makeFileContent(for: evaluation.tasks))\n\n"
+		+ makeFileContent(for: evaluation.populations)
+	}
+	
+	func makeFileContent(for tasks: [Task]) -> String {
+		return "Tasks: " + tasks.map({ "\($0.id)" }).joinWithSeparator(" ")
+	}
+	
+	func makeFileContent(for populations: [Population]) -> String {
+		return populations.enumerate().map({
+			"Population \($0.0)\n\(self.makeFileContent(for: $0.1))"
+		}).joinWithSeparator("\n\n")
+	}
+	
+	func makeFileContent(for population: Population) -> String {
+		return population.members.map(makeFileContent(for:)).joinWithSeparator("\n")
+	}
+	
+	func makeFileContent(for individual: Individual) -> String {
+		return "\(individual.fitness) \(individual.chromosome.stringValue)"
+	}
+	
+}
