12e8a558702c5f80402daec4bcec113d8b97ff50
Bryan Hoke <bryan.hoke@nextthought.com>
2016-06-10 11:51:51 -0500

    Modify the config that is output to file to better reflect the experiment condition.

--- EvolutionOfLearning/ExperimentRunner.swift
+++ EvolutionOfLearning/ExperimentRunner.swift
@@ -32,11 +32,24 @@ class ExperimentRunner<IndividualType : Individual> : ExperimentRunning {
 		set { config.evolutionaryTaskCount = newValue }
 	}
 	
+	private var configForWriting: ExperimentConfig {
+		var config = self.config
+		switch condition {
+		case .learningRuleEvolution:
+			config.fitnessConfig.bitsPerWeight = 0
+			config.fitnessConfig.encodingExponentShift = 0
+		case .networkEvolution:
+			config.fitnessConfig.learningRuleSize = 0
+			config.fitnessConfig.numberOfTrainingEpochs = 0
+		}
+		return config
+	}
+	
 	func runExperiment(using tasks: [Task]) {
 		srand48(Int(arc4random()))
 		
 		recorder?.createResultsDirectory()
-		recorder?.write(config)
+		recorder?.write(configForWriting)
 		
 		let experiment = makeExperiment(tasks: tasks)
 		experiment.run(forNumberOfTrials: numberOfTrials, onTrialComplete: {
@@ -46,6 +59,8 @@ class ExperimentRunner<IndividualType : Individual> : ExperimentRunning {
 		
 		recorder?.writeOverview()
 	}
+	
+	
 	
 	private func makeExperiment(tasks tasks: [Task]) -> AnyExperiment<Record> {
 		switch condition {
