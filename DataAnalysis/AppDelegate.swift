c3238d03115dd369844b97a941b70ac2bfeedcad
Bryan Hoke <bryan.hoke@nextthought.com>
2017-06-07 17:04:26 -0500

    Implement the DataAnalysis app behavior.

--- DataAnalysis/AppDelegate.swift
+++ DataAnalysis/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import Cocoa
 
+private let externalBasePath = "/Volumes/Seagate Blue/Thesis/"
+private let localBasePath = "/Users/bryanhoke/Projects/BDHSoftware/OS X/EvolutionOfLearning/"
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
@@ -16,6 +19,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
 		// Insert code here to initialize your application
+		
+		let dataSetDirectory = localBasePath + "Results/Set 14/"
+		
+		do {
+			let dataSet = try DataSetScanner.shared.scanDataSet(fromDirectoryAtPath: dataSetDirectory)
+			try ColumnwiseDataSetRecorder.shared.write(dataSet, toDirectoryAtPath: dataSetDirectory)
+		}
+		catch let error as NSError {
+			print(error)
+		}
+		
+		
+//		let experimentDirectory = "/Volumes/Seagate Blue/Thesis/Results/Set 11/Experiment 00/"
+//		let overview = try! ExperimentScanner.shared.scanExperimentOverview(fromDirectoryAtPath: experimentDirectory)
+		
+//		let trialPath = "/Volumes/Seagate Blue/Thesis/Results/Set 11/Experiment 00/Trial 0.txt"
+//		try! TrialScanner.shared.scanTrial(fromFileAtPath: trialPath)
 	}
 
 	func applicationWillTerminate(aNotification: NSNotification) {
