85e1088ef5bd9f84bfc6f0b66d22c454dca1ad35
Bryan Hoke <bryan.hoke@nextthought.com>
2017-06-14 12:45:08 -0500

    Add methods to ExperimentScanner for scanning ExperimentRecords.

--- DataAnalysis/ExperimentScanner.swift
+++ DataAnalysis/ExperimentScanner.swift
@@ -12,6 +12,38 @@ final class ExperimentScanner {
 	
 	static let shared = ExperimentScanner()
 	
+	// MARK: Scanning ExperimentRecords
+	
+	func scanExperimentRecord(fromDirectoryAtPath path: String) throws -> ExperimentRecord {
+		let url = URL(fileURLWithPath: path, isDirectory: true)
+		return try scanExperimentRecord(fromDirectoryAt: url)
+	}
+	
+	func scanExperimentRecord(fromDirectoryAt url: URL) throws -> ExperimentRecord {
+		let trials = try scanTrialRecords(fromDirectoryAt: url)
+		
+		var experiment = ExperimentRecord()
+		
+		for trial in trials {
+			experiment.trials.append(trial)
+		}
+		
+		return experiment
+	}
+	
+	fileprivate func scanTrialRecords(fromDirectoryAt url: URL) throws -> [TrialRecord] {
+		let trialURLs = try scanTrialURLs(fromDirectoryAt: url)
+		var trials = [TrialRecord]()
+		trials.reserveCapacity(trialURLs.count)
+		for url in trialURLs {
+			let trial = try TrialScanner.shared.scanTrialRecord(fromFileAtPath: url.path.removingPercentEncoding!)
+			trials.append(trial)
+		}
+		return trials
+	}
+	
+	// MARK: Scanning ExperimentOverviews
+	
 	func scanExperimentOverview(fromDirectoryAtPath path: String) throws -> ExperimentOverview {
 		let url = URL(fileURLWithPath: path, isDirectory: true)
 		return try scanExperimentOverview(fromDirectoryAt: url)
@@ -38,11 +70,12 @@ final class ExperimentScanner {
 			trials.append(trial)
 		}
 		return trials
-//		return try trialURLs.map { try TrialScanner.shared.scanTrial(fromFileAtPath: $0.resourceSpecifier.stringByRemovingPercentEncoding!) }
 	}
 	
+	// MARK: Helpers
+	
+	/// Returns the URLs of all the trial files contained in the directory at the given URL.
 	fileprivate func scanTrialURLs(fromDirectoryAt url: URL) throws -> [URL] {
-//		let url = NSURL(fileURLWithPath: path, isDirectory: true)
 		let fileManager = FileManager()
 		let directoryContents = try fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: [URLResourceKey.nameKey], options: [])
 		return directoryContents.filter { $0.lastPathComponent.hasPrefix("Trial") }
